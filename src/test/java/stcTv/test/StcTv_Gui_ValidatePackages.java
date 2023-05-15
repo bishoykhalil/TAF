07365d6032ee3139abebb739d75ae2382bcba981
Bishoy Khalil helmy <74961440+bishoykhalil@users.noreply.github.com>
2023-05-15 23:12:45 +0300

    STC_TV Packages-3

--- src/test/java/stcTv/test/StcTv_Gui_ValidatePackages.java
+++ src/test/java/stcTv/test/StcTv_Gui_ValidatePackages.java
@@ -1,25 +1,27 @@
 package stcTv.test;
 
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import stctv.gui.Bulider.PackageBulider;
 import stctv.gui.pages.StcTv_HomePage;
 import stctv.gui.pages.StcTv_PackagesPage;
 import utils.enums.stcTv.Country;
+import utils.enums.stcTv.Currency;
 import utils.enums.stcTv.TVPackages;
 
 public class StcTv_Gui_ValidatePackages {
 
 
     @Test
-    public void stcTv_gui_ValidatePackages(){
-        new StcTv_HomePage().navigateToHomePage().selectCountry(Country.fromString("KSA"));
+    @Parameters({"Country" , "Package"})
+    public void stcTv_gui_ValidatePackages( Country country,TVPackages tvPackage){
+        new StcTv_HomePage().navigateToHomePage().selectCountry(country);
         StcTv_PackagesPage packagesPage= new StcTv_PackagesPage();
-        Assert.assertTrue(packagesPage.getPackageName(TVPackages.CLASSIC).equalsIgnoreCase(TVPackages.CLASSIC.getPackageName()));
-        Assert.assertTrue(packagesPage.getPackagePrice(TVPackages.CLASSIC).equalsIgnoreCase(TVPackages.CLASSIC.getPrice()));
-        //Assert.assertTrue(packagesPage.getPackageCurrency(TVPackages.CLASSIC).equalsIgnoreCase(TVPackages.CLASSIC.getPackageName()));
+        Assert.assertTrue(packagesPage.getPackageName(tvPackage).equalsIgnoreCase(tvPackage.getPackageName()));
+        Assert.assertTrue(packagesPage.getPackagePrice(tvPackage).equalsIgnoreCase(tvPackage.getPrice()));
+        Currency currency =  country.equals(Country.KSA) ? Currency.SAR :
+                country.equals(Country.BAHRAIN) ? Currency.BHD :
+                        country.equals(Country.KUWAIT) ? Currency.KWD : null;
+        Assert.assertTrue(packagesPage.getPackageCurrency(TVPackages.CLASSIC).equalsIgnoreCase(currency.getCurrencyName()));
     }
 }
